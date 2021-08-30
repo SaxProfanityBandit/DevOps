8f39887ad1ab3ce0331a05c3b5d836d414ec8995
SaxProfanityBandit <rsahlgren@gmail.com>
2021-08-30 11:01:33 +0200

    The second exercise during the lession. First commit.

---
+++ PythonLession1/intro_python.py
@@ -0,0 +1,8 @@
+intro_python = "Avklarat"
+exercises = 3
+points_per_exercise = 5
+point_total = 100
+
+point_total += (exercises * points_per_exercise)
+
+print("I got " + str(point_total) + " points!")
