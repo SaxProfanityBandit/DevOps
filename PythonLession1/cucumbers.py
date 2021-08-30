9b5e5827c60c759d491a34cec6076e5f5c406400
SaxProfanityBandit <rsahlgren@gmail.com>
2021-08-30 10:52:06 +0200

    Updated the lession exercise with comments.

--- PythonLession1/cucumbers.py
+++ PythonLession1/cucumbers.py
@@ -1,4 +1,4 @@
-cucumbers = 11
-cucumber_price = 8.5
-total_cost = cucumbers * cucumber_price
-print("The total cost of " + str(cucumbers) + " cucumbers is " + str(total_cost) + " KR")
+cucumbers = 11 #Constant of how many cucumbers we are buying.
+cucumber_price = 8.5 #The price per cucumber in SEK.
+total_cost = cucumbers * cucumber_price #The total cost of the purchase.
+print("The total cost of " + str(cucumbers) + " cucumbers is " + str(total_cost) + " KR") #The formating and printing of the lession assignment.
