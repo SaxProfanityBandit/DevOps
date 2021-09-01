80d4bf9fba79d42b2b30ceaa5c04402eab4db13e
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-01 14:12:17 +0200

    Added shebang and changed bit permissions to execute, also added comments.

--- PythonLession2/potatoes.py
+++ PythonLession2/potatoes.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python3
+
+
+#User Input
 potatoes = int(input("How many potatoes do you have? "))
 nr_of_persons = int(input("How many people are arriving? "))
 
+#Calculating each people's portion.
 potatoes_each = potatoes / nr_of_persons
 print("Each person gets {} potatoes each.".format(potatoes_each))
