c2f622ba6eceb83d02f51c93581856b974157fe8
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-01 16:00:57 +0200

    Different way of inputing, will simplify calculation later.

--- PythonLession2/month_calculation.py
+++ PythonLession2/month_calculation.py
@@ -1,8 +1,13 @@
 from datetime import datetime
 from datetime import date
 #from dateutil import relativedelta
-birthdate = input("Please input your day of birth(dd/mm/yyyy): ")
+#birthdate = input("Please input your day of birth(dd/mm/yyyy): ")
+birthyear = input("Please enter your birthyear: ")
+birthmonth = input("Please enter your birth month: ")
+
+birthdate = "{}/{}/{}".format(1, birthmonth, birthyear)
 start_date = datetime.strptime(birthdate,'%d/%m/%Y')
+
 end_date = datetime.today()
 difference = (abs(((start_date.year * 12) + start_date.month) - ((end_date.year * 12) + start_date.month)))
 print("You are {} months old!".format(difference))
