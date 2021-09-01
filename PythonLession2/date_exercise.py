de45af3ffdf7f71af66214e1a720d2ff6a98f74b
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-01 11:44:17 +0200

    Added a more compact way and more industry way to format the dates

--- PythonLession2/date_exercise.py
+++ PythonLession2/date_exercise.py
@@ -1,4 +1,8 @@
 from datetime import datetime
 now = datetime.now()
 
+#Lession Taught Way
 print("{}/{}/{} {}:{}:{}".format(now.day, now.month, now.year, now.hour, now.minute, now.second))
+
+#Industry way:
+print(now.strftime('%d/%m/%Y %H:%M:%S'))
