1c30fab44e010f9a448d5fd3041320fdb4907e48
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-01 13:54:20 +0200

    Correction of if-statements as we go from above and down.

--- PythonLession2/grading.py
+++ PythonLession2/grading.py
@@ -1,11 +1,11 @@
 def grade_converter(grade):
 	if grade >= 90:
 		return "A"
-	elif grade >= 80 and grade < 90:
+	elif grade >= 80: #and grade < 90:
 		return "B"
-	elif grade >= 70 and grade < 80:
+	elif grade >= 70: #and grade < 80:
 		return "C"
-	elif grade >= 65 and grade < 70:
+	elif grade >= 65: #and grade < 70:
 		return "D"
 	else:
 		return "F"
