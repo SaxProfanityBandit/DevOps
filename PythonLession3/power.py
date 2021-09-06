0b3c20cf7ea2de2ae12acca9a418f23d98e4aa64
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-06 09:34:02 +0200

    Updated with another exercise in this file.

--- PythonLession3/power.py
+++ PythonLession3/power.py
@@ -4,9 +4,16 @@ def power(base, exponent):
 
 def power_return(base, exponent):
 	return base ** exponent
+#The term doubler is wrong, it multiplies with itself but follow assignment.
+def doubler(number):
+	return power_return(number, 2)
 
-power(2, 3)
-power(4, 4)
-power(10, 3)
+print("3 multiplied by itself is {}".format(doubler(3)))
+print("5 multiplied by itself is {}".format(doubler(5)))
 
-print("The power of 2 to 8 is {}".format(power_return(2, 8)))
+
+#power(2, 3)
+#power(4, 4)
+#power(10, 3)
+
+#print("The power of 2 to 8 is {}".format(power_return(2, 8)))
