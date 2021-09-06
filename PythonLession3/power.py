8872f341263924188c1765bd528a5be8a65094e8
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-06 09:18:07 +0200

    I added a version of the function that returns rather than prints from the function.

--- PythonLession3/power.py
+++ PythonLession3/power.py
@@ -2,7 +2,11 @@ def power(base, exponent):
 	result = base ** exponent
 	print("{} to the power of {} is {}.".format(base, exponent, result))
 
+def power_return(base, exponent):
+	return base ** exponent
+
 power(2, 3)
 power(4, 4)
 power(10, 3)
-	
+
+print("The power of 2 to 8 is {}".format(power_return(2, 8)))
