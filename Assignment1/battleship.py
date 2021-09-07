a944f3901bebdef9a680daf55b3344e404e18967
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-07 15:27:20 +0200

    More progress on hotseat multiplayer, trying to figure out why its not taking turns.

--- Assignment1/battleship.py
+++ Assignment1/battleship.py
@@ -65,43 +65,47 @@ def game(players):
 	for player in range(players):
 		player_list.append(guesses)
 	print(player_list)
+	player = 1
 	#User Input
-	while:
-		guess_row = input("Guess row: ")
-		guess_column = input("Guess column: ")
-		
-		#Sanity checking
-		if len(guess_row) == 0:
-			print("You didn't enter a number in the row guess!")
-			guess_row = input("Guess row: ")
-		elif len(guess_column) == 0:
-			print("You didn't enter a number in the column guess!")
-			guess_column = input("Guess column: ")
-		else:
-			print("Good luck!")
-		
-		guess_row = int(guess_row) - 1
-		guess_column = int(guess_column) - 1
-
-		if guess_row > len(board):
-			print("Outside of bounds, try again!")
-		elif guess_column > len(board[guess_row]):
-			print("Outside of bounds, try again!")
-		elif board[guess_row][guess_column] == "S":
-			print("Congratulations, you sank the ship!")
-			print_board(board)
-			return True
-		else:
-			guesses -= 1
-			if board[guess_row][guess_column] == "X":
-				print("You've already guessed that!")
+	while player-1 < len(player_list):
+		print(player)
+		while player_list[player-1] > 0:
+			guess_row = input("Player {}: Guess row: ".format(player))
+			guess_column = input("Player {}: Guess column: ".format(player))
+			
+			#Sanity checking
+			if len(guess_row) == 0:
+				print("You didn't enter a number in the row guess!")
+				guess_row = input("Guess row: ")
+			elif len(guess_column) == 0:
+				print("You didn't enter a number in the column guess!")
+				guess_column = input("Guess column: ")
 			else:
-				board[guess_row][guess_column] = "X"
-			print("Sorry, you missed! Try again! You have {} guesses left.".format(guesses))
-	if guesses == 0:
-		print("Game over! No more guesses")
-		print_board(board)
-		return False
+				print()
+			
+			guess_row = int(guess_row) - 1
+			guess_column = int(guess_column) - 1
+
+			if guess_row > len(board):
+				print("Player {}: Outside of bounds, try again!".format(player))
+			elif guess_column > len(board[guess_row]):
+				print("Player {}: Outside of bounds, try again!".format(player))
+			elif board[guess_row][guess_column] == "S":
+				print("Player {}: Congratulations, you sank the ship!".format(player))
+				print_board(board)
+				return True
+			else:
+				player_list[player] -= 1
+				if board[guess_row][guess_column] == "X":
+					print("You've already guessed that!")
+				else:
+					board[guess_row][guess_column] = "X"
+				print("Player {}: Sorry, you missed! Try again! You have {} guesses left.".format(player, player_list[player]))
+		if player_list[player] == 0:
+			print("Player {}: Game over! No more guesses".format(player))
+			print_board(board)
+			return False
+		player += 1
 
 
 game(int(input("How many players(1/2)? ")))
