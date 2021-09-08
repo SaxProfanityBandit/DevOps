81b667e512853a9692a00a147c5619c28408bc06
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-08 11:29:07 +0200

    Added comments to former exercise, also learned that range doesnt actually create a list.

--- PythonLession4/for_loops.py
+++ PythonLession4/for_loops.py
@@ -1,7 +1,9 @@
+#Is actually not a list. Learned that.
 numbers = range(0, 100)
 for number in numbers:
 	if number % 2 == 0:
 		print("{} is even.".format(number))
 	else:
 		print("{} is not even.".format(number))
+
 
