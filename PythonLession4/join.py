110497059e397ffd7d9d694b4f291e370f6c43a8
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-08 16:43:01 +0200

    Updated a few files.

---
+++ PythonLession4/join.py
@@ -0,0 +1,8 @@
+def join(x, y):
+	return x + y
+
+#rad = join(["Hej/", "Hi/", "Bonjour "], "3")
+#print(rad) 
+
+
+print(join(['Hej! ', 'Allesamma '], ['Va trevligt ', 'att se er alla']))
--- PythonLession4/run_arguments.py
+++ PythonLession4/run_arguments.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+import os
+
 #Is this the running file or a module? Retreive arguments if its the former.
 if __name__ == '__main__':
 	import sys
@@ -8,5 +11,15 @@ def print_args(*args):
 	for argument in args:
 		print(argument)
 
+path = arguments[0]
+file_to_find = arguments[1]
 
-print_args(arguments)
+#os.system('ls {}'.format(path))
+
+if os.path.isfile("{}/{}".format(path, file_to_find)) == True:
+	print("File found!")
+	print(os.path.isfile("{}/{}".format(path, file_to_find)))
+	print("{}/{}".format(path, file_to_find))
+else:
+	print("Couldn't find file.")
+
