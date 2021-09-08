c926f29dcfa03dc445beb449bb0e6a2ad28316ff
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-08 11:55:16 +0200

    Updated the for_loops.py for another exercise.

--- PythonLession4/for_loops.py
+++ PythonLession4/for_loops.py
@@ -1,9 +1,21 @@
 #Is actually not a list. Learned that.
-numbers = range(0, 100)
-for number in numbers:
-	if number % 2 == 0:
-		print("{} is even.".format(number))
-	else:
-		print("{} is not even.".format(number))
+def check_even():
+	numbers = range(0, 100)
+	for number in numbers:
+		if number % 2 == 0:
+			print("{} is even.".format(number))
+		else:
+			print("{} is not even.".format(number))
 
+def almost_total(numbers):
+	sum_of_list = []
+	for i in range(0, len(numbers)):
+		if i > 0 and i < len(numbers)-1:
+			sum_of_list.append(numbers[i])
+			print(numbers[i])
+	return sum(sum_of_list)
+	#Doing this way to practice/show the sum function.
 
+print(almost_total([1, 2, 4, 5]))
+		
+		
