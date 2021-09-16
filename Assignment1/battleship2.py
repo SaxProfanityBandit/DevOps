7b077609013aada5a7e0e9c18d0c71d7284eb9e3
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-16 11:19:09 +0200

    More progress of the rewrite of battleship, currently working on input handling

--- Assignment1/battleship2.py
+++ Assignment1/battleship2.py
@@ -12,16 +12,18 @@ Defining the Game class to make it easier to
 understand the code further in the project
 """
 class Game(object):
-        #Stuff here
+    guess_row = 1
+    guess_col = 1
+
     def __init__(self, players):
         self.guesses = 5
         self.player_list = []
         for player in range(players):
                 self.player_list.append(self.guesses)
-        self.players = 1
+        self.current_player = 1
         self.board = self.create_matrix(5, 5)
-        self.ship_row = 1
-        self.ship_col = 1
+        self.ship_row = rand.randint(0, 4)
+        self.ship_col = rand.randint(0, 4)
     
     """
     Defining the many methods that makes the game work,
@@ -46,20 +48,57 @@ class Game(object):
     def print_board(self):
         x = 0
         y = 0
-        for row in self.board:
+        for column in self.board:
             y = 0
-            for column in row:
+            for row in column:
                 if y == 0:
-                    print(" ",column, end=" ")
+                    print(" ",row, end=" ")
                 elif y == len(self.board[x]):
-                    print("", column, end="")
+                    print("", row, end="")
                 else:
-                    print(column, end=" ")
+                    print(row, end=" ")
                 y += 1
             print()
             x += 1
         return None
 
+    """def user_input(self):
+        print("Player {}: Guess row: ".format(self.current_player))
+        guess_row = input("\n")
+        if len(guess_row) != 0:
+            if int(guess_row) > len(self.board):
+                print("That's not even in the ocean! Try again.")
+                return self.user_input()
+            else:
+                self.guess_row = int(guess_row)
+        else:
+            print("You didn't type anything. Try again!")
+            return self.user_input()
+
+        print("Player {}: Guess column: ".format(self.current_player))
+        guess_col = input("\n")
+        if len(guess_col) != 0:
+            if int(guess_col) > len(self.board[int(guess_row)]):
+                print("That's not even in the ocean! Try again.")
+                return self.user_input()
+            else:
+                self.guess_col = int(guess_col)
+        else:
+            print("You didn't type anything. Try again!")
+            return self.user_input()
+        """
+            
+
+
+
+
+    def main(self):
+        os.system("clear")
+
+
+
+
 
 battleship = Game(1)
 battleship.print_board()
+battleship.user_input()
