7c7f8ab383f7f3155a5ece1133474381b0e58b51
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-20 12:15:09 +0200

    Update changes.

--- PythonLession7/computer_statistics.py
+++ PythonLession7/computer_statistics.py
@@ -6,12 +6,12 @@ import psutil
 class CPUSnapshot(object):
 	def __init__(self):
 		self.cpu_usage = psutil.cpu_percent()
-		self.memory_usage = psutil.virtual_memory().used
+		self.memory_usage = psutil.virtual_memory().used / 1024
 		self.disk_usage = psutil.disk_usage('/').used / 1024
-	def get_snapshot_str(self):
-		return "CPU Usage: {}% of 100%.\nMemory Used: {}\nDisk Usage: {}".format(self.cpu_usage, self.memory_usage, self.disk_usage)
+	def __repr__(self):
+		return "CPU Usage: {}% of 100%.\nMemory Used: {} MBs\nDisk Usage: {} MBs".format(self.cpu_usage, self.memory_usage, self.disk_usage)
 
 
 snapshot = CPUSnapshot()
-print(snapshot.get_snapshot_str())
+print(snapshot)
 
