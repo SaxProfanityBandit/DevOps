c10b440a1d6987b94c7921bcaec861dc92e73422
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-26 16:57:26 +0200

    Started on second assignment, need to check with teacher on requirements.

---
+++ Assignment2/KillByName/kill_by_name.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""Imports"""
+
+import os
+import sys
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    args = ""
+
+    """Handles multiple arguments for different processes."""
+    if len(arguments) > 1:
+        for argument in arguments:
+            args += " -e " + argument
+    else:
+        args = arguments[0]
+
+"""Instead of a loop through the entire process list, I grab what the user is looking for directly."""
+process_raw = os.popen('ps aux | grep {}'.format(args)).read().splitlines()
+
+"""Formatting"""
+formated_processes = list()
+
+for line in process_raw:
+        process = line.split()
+        formated_processes.append(
+            {
+                'pid'     : process[1],
+                'user'    : process[0],
+                'cpu'     : process[2],
+                'mem'     : process[3],
+                'started' : process[4],
+                'elapsed' : process[5],
+                'path'    : process[6]
+            }
+        )
+
+for dict in formated_processes:
+    print(dict)
