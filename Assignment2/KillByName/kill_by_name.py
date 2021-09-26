54699e6d99578efcc43dffcde4d6c52aa849e36a
SaxProfanityBandit <rsahlgren@gmail.com>
2021-09-26 18:08:21 +0200

    First done prototype of kill_by_name. No classes or several files as assigment asks for.

--- Assignment2/KillByName/kill_by_name.py
+++ Assignment2/KillByName/kill_by_name.py
@@ -19,33 +19,45 @@ if __name__ == "__main__":
 """Instead of a loop through the entire process list, I grab what the user is looking for directly."""
 process_raw = os.popen('ps aux | grep {}'.format(args)).read().splitlines()
 
-"""Formatting"""
+"""Formatting and filtering"""
 formated_processes = list()
 
 for line in process_raw:
-        process = line.split()
-        formated_processes.append(
-            {
-                'pid'     : process[1],
-                'user'    : process[0],
-                'cpu'     : process[2],
-                'mem'     : process[3],
-                'started' : process[8],
-                'elapsed' : process[9],
-                'path'    : process[10]
-            }
-        )
 
+    if "python3" in line or "/bin/sh" in line or "grep" in line:
+        continue
+
+    process = line.split()
+    line_args = process[11:]
+    arg_string = ""
+    for arg in line_args:
+        arg_string += arg + " "
+
+    formated_processes.append(
+        {
+            'pid'     : process[1],
+            'user'    : process[0],
+            'cpu'     : process[2],
+            'mem'     : process[3],
+            'started' : process[8],
+            'elapsed' : process[9],
+            'path'    : process[10],
+            'args'    : arg_string                    
+        }
+    )
 
 """Kill logic and printing."""
-if len(arguments) > 1:
+if len(formated_processes) > 1:
     for process in formated_processes:
         print(
-        "[{}] {} (User) {}% (CPU) {}% (Mem) {} (Started) {} (Elapsed) {}".format(
-        process['pid'], process['user'], process['cpu'], process['mem'], process['started'], process['elapsed'], process['path']
+        "[{}] {} (User) {}% (CPU) {}% (Mem) {} (Started) {} (Elapsed) {} {}".format(
+        process['pid'], process['user'], process['cpu'], process['mem'], process['started'], process['elapsed'], process['path'], process['args']
         )
     )
-else:
-    #print("pkill {}".format(formated_processes[0]['pid']))
+elif len(formated_processes) == 1:
     os.system("kill {}".format(formated_processes[0]['pid']))
-    print("Process killed!")
+    print("Terminated process with PID: {}".format(formated_processes[0]['pid']))
+else:
+    print("Couldn't find the process you were looking for!")
+    
+    
